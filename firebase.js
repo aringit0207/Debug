68453c48a5313616b5aa5a9e4e319df27ae6d7de
Arin Jain <arinjain1610@gmail.com>
2025-03-08 00:21:26 +0530

    made firebaseConfig more secured

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.FIREBASE_API_KEY,
-    authDomain: "mellow-92950.firebaseapp.com",
-    projectId: "mellow-92950",
-    storageBucket: "mellow-92950.firebasestorage.app",
-    messagingSenderId: "216617044702",
-    appId: "1:216617044702:web:83fa9ece066ab52812904f",
-    measurementId: "G-70BZH203QW"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
